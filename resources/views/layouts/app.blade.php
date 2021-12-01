52abf4a33826b77f130119a40acd6d7e25785e12
localedev <asaraach@gmail.com>
2021-12-01 20:58:44 +0100

    up

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,39 +33,13 @@
                   'current_language' => LaravelLocalization::getCurrentLocale(),
           ]) !!};
     </script>
-
-    <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-176648-2"></script>
-    <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-
-        gtag('config', 'UA-176648-2');
-    </script>
-    <!-- Global site tag (gtag.js) - Google Ads: 1068260209 -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=AW-1068260209"></script>
-    <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-
-        gtag('config', 'AW-1068260209');
-    </script>
-    {{-- <script src="https://cdn.jsdelivr.net/npm/js-cookie@rc/dist/js.cookie.min.js"></script> --}}
-
-    @livewireStyles
     @livewireScripts
-    {{-- <script src="https://cdn.jsdelivr.net/gh/livewire/vue@v0.3.x/dist/livewire-vue.js"></script> --}}
-    {{-- <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.8.2/dist/alpine.min.js" defer></script> --}}
     <script src="https://cdn.jsdelivr.net/gh/livewire/turbolinks@v0.1.x/dist/livewire-turbolinks.js" data-turbolinks-eval="false"></script>
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
     <script src="{{ asset('js/trans/text_'.LaravelLocalization::getCurrentLocale().'.js') }}"></script>
     <script defer src="{{ mix('js/player.js') }}" data-turbolinks-eval="false"></script>
     <script src="{{ mix('js/app.js') }}" defer data-turbolinks-eval="true" data-turbolinks-track="reload"></script>
-
-    {{-- <script src="{{ mix('js/turbolinks.js') }}" defer></script> --}}
     @yield('headerScripts')
 </head>
 
@@ -88,20 +62,6 @@
         </div>
     </div>
     @endif
-
-    <!-- Begin Inspectlet Asynchronous Code -->
-    {{-- <script type="text/javascript">
-        (function() {
-        window._insp = window._insp || [];
-        __insp.push(['wid', 1531425017]);
-        var ldinsp = function(){
-        if(typeof window._inspld != "undefined") return; window._inspld = 1; var insp = document.createElement('script'); insp.type = 'text/javascript'; insp.async = true; insp.id = "inspsync"; insp.src = ('https:' == document.location.protocol ? 'https' : 'http') + '://cdn.inspectlet.com/inspectlet.js?wid=1531425017&r=' + Math.floor(new Date().getTime()/3600000); var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(insp, x); };
-        setTimeout(ldinsp, 0);
-        })();
-    </script> --}}
-    <!-- End Inspectlet Asynchronous Code -->
-
-    <script src="https://cdn.jsdelivr.net/npm/howler@2.2.1/dist/howler.min.js"></script>
 
     @yield('page-scripts')
 </body>
